60e03bd6764affb3b792def095b854020e866c8d
Jon Garrett <jonathan.garrett@gmail.com>
2020-09-13 18:59:35 -0600

    Use lodash memoize to cache GitHub query [TODO]Clear cache function

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,24 @@
 import express from 'express'
+import axios, { AxiosRequestConfig } from 'axios'
+import _ from 'lodash'
+
 const app = express()
+
 const port = 4000
 
+const get = _.memoize(axios.get)
+
 // path for all api routes
-app.get('/api', (req, res) => {
-    res.send('Pretend this is an API response')
+app.get('/api', async (req, res) => {
+    const example = await get(
+        'https://api.github.com/search/repositories?page=3&per_page=100',
+        {
+            params: {
+                q: 'well hello there',
+            },
+        }
+    )
+    res.send(example.data)
 })
 
 app.listen(port, () => {
