7fcdd2faf3a7886b73a01377b133aa77e41ec278
Jon Garrett <jonathan.garrett@gmail.com>
2020-09-14 21:46:19 -0600

    Add details component view

--- client/src/Details.tsx
+++ client/src/Details.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { IResponseItem } from './types'
 
-const Details = () => (
+interface IProps {
+    selected: IResponseItem | null
+}
+
+const Details = ({ selected }: IProps) => (
     <div data-testid="details-page-container">
         <h1>Details Page</h1>
+        {selected ? (
+            <>
+                <h2>Name: {selected.name}</h2>
+                <h2>Description: {selected.description}</h2>
+
+                <h2>Stars: {selected.stargazers_count}</h2>
+                <h2>Language: {selected.language}</h2>
+                <h2>Owner: {selected.owner.login}</h2>
+            </>
+        ) : (
+            <h2>Please return to home page and create a search first</h2>
+        )}
     </div>
 )
 
