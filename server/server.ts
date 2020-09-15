975642f6baabc3262147bd1020a2c986dfeba75c
Jon Garrett <jonathan.garrett@gmail.com>
2020-09-14 21:43:28 -0600

    Fix cache and implement some query params

--- server/server.ts
+++ server/server.ts
@@ -1,28 +1,56 @@
 import express from 'express'
-import axios, { AxiosRequestConfig } from 'axios'
+import axios from 'axios'
+import url from 'url'
 import _ from 'lodash'
 
 const app = express()
 
 const port = 4000
 
-// [TODO] Clear cache occasionally
-const get = _.memoize(axios.get)
+const cache: { [query: string]: any } = {}
+
+interface QueryObject {
+    description?: string
+    language?: string
+    name?: string
+    default?: string
+}
 
 app.get('/api', async (req, res) => {
-    try {
-        const example = await get(
-            'https://api.github.com/search/repositories',
-            {
-                params: {
-                    q: req.query.query,
-                },
+    const query: QueryObject = req.query
+    // Should do more validation here for user input
+    if (query) {
+        const stringified = Object.values(query).join('-')
+        const cached = cache[stringified]
+        if (cached && Date.now() - cached.time < 1000 * 60 * 60) {
+            res.send(cached.data)
+        } else {
+            try {
+                let formattedQuery
+                if (query.default) {
+                    formattedQuery = query.default
+                } else {
+                    formattedQuery = `${query.name}+in:name+language:${query.language}+${query.description}+in:description`
+                }
+                const gitRes = await axios.get(
+                    'https://api.github.com/search/repositories',
+                    {
+                        params: {
+                            q: formattedQuery,
+                        },
+                    }
+                )
+                cache[stringified] = {
+                    data: gitRes.data,
+                    time: Date.now(),
+                }
+
+                res.send(gitRes.data)
+            } catch (e) {
+                console.log('error: ', e)
+                res.send(e)
             }
-        )
-        res.send(example.data)
-    } catch (e) {
-        console.log('error: ', e)
-        res.send(e)
+        }
     }
 })
 
